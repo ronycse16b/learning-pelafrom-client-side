18f159ae813f8c66b4c94d3ec07b6f4a54f90217
ronycse16b <rony.cse16b@gmail.com>
2022-10-28 18:41:50 +0600

    error page implement

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
         <div>
-            <section className="flex items-center h-full p-16 dark:bg-gray-900 dark:text-gray-100">
+            <section className="flex items-center h-full p-16 bg-red-600 dark:bg-gray-900 dark:text-gray-100 text-white">
                 <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
                     <div className="max-w-md text-center">
                         <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
-                            <span className="sr-only">Error</span>404
+                            <span className="sr-only text-red-600 font-bold">Error</span>404
                         </h2>
-                        <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
-                        <p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
-                        <a rel="noopener noreferrer" href="#" className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</a>
+                        <p className="text-2xl font-semibold md:text-3xl">Oh! Here isnt Any Page.</p>
+                        <p className="mt-4 mb-8 dark:text-gray-400"> Go homepage</p>
+                        <Link to ='/' rel="noopener noreferrer" href="#" className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</Link>
                     </div>
                 </div>
             </section>
