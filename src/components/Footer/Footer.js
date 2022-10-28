50360c3aa2b8402b958287618fad7f903193dd67
ronycse16b <rony.cse16b@gmail.com>
2022-10-28 18:50:27 +0600

    footer added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
 						<path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
 					</svg> 
 				</div>
-				<span className="self-center text-2xl font-semibold">Brand name</span>
+				<span className="self-center text-2xl font-semibold">E-GRAPHICES IT</span>
 			</a>
 		</div>
 		<div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
@@ -20,13 +20,13 @@ const Footer = () => {
 				<h3 className="tracking-wide uppercase dark:text-gray-50">Product</h3>
 				<ul className="space-y-1">
 					<li>
-						<a rel="noopener noreferrer" href="#">Features</a>
+						<a rel="noopener noreferrer" href="#">Home</a>
 					</li>
 					<li>
-						<a rel="noopener noreferrer" href="#">Integrations</a>
+						<a rel="noopener noreferrer" href="#">Course</a>
 					</li>
 					<li>
-						<a rel="noopener noreferrer" href="#">Pricing</a>
+						<a rel="noopener noreferrer" href="#">Blog</a>
 					</li>
 					<li>
 						<a rel="noopener noreferrer" href="#">FAQ</a>
@@ -34,7 +34,7 @@ const Footer = () => {
 				</ul>
 			</div>
 			<div className="space-y-3">
-				<h3 className="tracking-wide uppercase dark:text-gray-50">Company</h3>
+				<h3 className="tracking-wide uppercase dark:text-gray-50">E Graphices IT</h3>
 				<ul className="space-y-1">
 					<li>
 						<a rel="noopener noreferrer" href="#">Privacy</a>
@@ -80,7 +80,7 @@ const Footer = () => {
 			</div>
 		</div>
 	</div>
-	<div className="py-6 text-sm text-center dark:text-gray-400">© 1968 Company Co. All rights reserved.</div>
+	<div className="py-6 text-sm text-center dark:text-gray-400">© 1968  Company Co. All rights reserved by Rony.</div>
 </footer>
         </div>
     );
